a903dc4e531aab01091d363c561b3a38d4155ddb
username91355 <personalmailD13@gmail.com>
2023-06-11 18:17:11 +0300

    BREAK(project): refactoring

---
+++ src/07-shared/ui/app-button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="app_button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "app-button",
+}
+</script>
+
+<style scoped>
+.app_button {
+  background: none;
+  border: 1px seagreen solid;
+  border-radius: 4px;
+  padding: 4px 8px;
+}
+</style>
---
+++ src/07-shared/ui/index.js
@@ -0,0 +1,7 @@
+import AppButton from './app-button.vue';
+import AppTag from './AppTag/AppTag.vue';
+
+export default [
+    AppButton,
+    AppTag
+]
